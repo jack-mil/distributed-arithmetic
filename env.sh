7621517bce65f69c2c293a89120b14a2e57291f6
jack-mil <62065280+jack-mil@users.noreply.github.com>
2025-04-05 12:22:25 -0400

    build: add vivado env setup

---
+++ env.sh
@@ -0,0 +1,10 @@
+##############################################################
+# Copyright (c) 1986-2025 Xilinx, Inc.  All rights reserved. #
+##############################################################
+
+export XILINX_VIVADO='C:\Xilinx\Vivado\2024.2'
+if [ -n "${PATH}" ]; then
+  export PATH='C:\Xilinx\Vivado\2024.2\bin:C:\Xilinx\Vivado\2024.2\lib\win64.o':$PATH
+else
+  export PATH='C:\Xilinx\Vivado\2024.2\bin:C:\Xilinx\Vivado\2024.2\lib\win64.o'
+fi
