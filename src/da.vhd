7a53a0a109b79e859131f61692fa72054dbfc884
jack-mil <62065280+jack-mil@users.noreply.github.com>
2025-04-05 13:58:14 -0400

    feat: attempt shifting and addition (not working)

--- src/da.vhd
+++ src/da.vhd
@@ -31,34 +31,34 @@ architecture arch of da is
   -- ----------------- Define Internal Signals -----------------
   -- --------- ROM ----------
   -- A0=7, A1=3, A2=-8, A3=-5
-  constant A0 : signed(3 downto 0) := to_signed(7, 4);
-  constant A1 : signed(3 downto 0) := to_signed(3, 4);
-  constant A2 : signed(3 downto 0) := to_signed(-8, 4);
-  constant A3 : signed(3 downto 0) := to_signed(-5, 4);
+  constant A0 : integer := 7;
+  constant A1 : integer := 3;
+  constant A2 : integer := -8;
+  constant A3 : integer := -5;
 
   function ROM(addr : unsigned(3 downto 0)) return signed is
-    variable result : signed(4 downto 0);
+    variable result : integer;
   begin
       case addr is
-          when "0000" => result := "00000";
-          when "0001" => result := A3;
-          when "0010" => result := A2;
-          when "0011" => result := A2 + A3;
-          when "0100" => result := A1;
-          when "0101" => result := A1 + A3;
-          when "0110" => result := A1 + A2;
-          when "0111" => result := A1 + A2 + A3;
-          when "1000" => result := A0;
-          when "1001" => result := A0 + A3;
-          when "1010" => result := A0 + A2;
-          when "1011" => result := A0 + A2 + A3;
-          when "1100" => result := A0 + A1;
-          when "1101" => result := A0 + A1 + A3;
-          when "1110" => result := A0 + A1 + A2;
-          when "1111" => result := A0 + A1 + A2 + A3;
-          when others => result := "00000";
+          when "0000" => result := 0;
+          when "0001" => result := A3; -- -5
+          when "0010" => result := A2; -- -8
+          when "0011" => result := A2 + A3; -- -13
+          when "0100" => result := A1; -- +3
+          when "0101" => result := A1 + A3; -- -2
+          when "0110" => result := A1 + A2; -- -5
+          when "0111" => result := A1 + A2 + A3; -- -10
+          when "1000" => result := A0; -- +7
+          when "1001" => result := A0 + A3; -- +2
+          when "1010" => result := A0 + A2; -- -1
+          when "1011" => result := A0 + A2 + A3; -- -6
+          when "1100" => result := A0 + A1; -- +10
+          when "1101" => result := A0 + A1 + A3; -- +5
+          when "1110" => result := A0 + A1 + A2; -- +2
+          when "1111" => result := A0 + A1 + A2 + A3; -- -3
+          when others => result := 0;
       end case;
-      return result;
+      return to_signed(result, 5); -- 5 bits needed
   end function;
 
   -- type array16x5b_t is array (0 to 15) of signed(4 downto 0);
@@ -82,20 +82,20 @@ architecture arch of da is
   -- );
 
   -- --------- Stage 1 --------
-  signal data_0_p1 : signed (3 downto 0) := (others => '0');
-  signal data_1_p1 : signed (3 downto 0) := (others => '0');
-  signal data_2_p1 : signed (3 downto 0) := (others => '0');
-  signal data_3_p1 : signed (3 downto 0) := (others => '0');
+  signal data_0_p1 : signed(3 downto 0) := (others => '0');
+  signal data_1_p1 : signed(3 downto 0) := (others => '0');
+  signal data_2_p1 : signed(3 downto 0) := (others => '0');
+  signal data_3_p1 : signed(3 downto 0) := (others => '0');
   signal valid_p1  : std_logic := '0'; -- stage one data is valid
   signal stall_p1  : std_logic := '0'; -- prevent more input while processing
-  signal count_p1  : unsigned (1 downto 0) := "00";
+  signal count_p1  : unsigned(1 downto 0) := "00";
 
   signal addr      : unsigned(3 downto 0); -- current input bits
   signal data_lut  : signed(4 downto 0); -- look-up data
 
   -- --------- Stage 2 --------
-  signal acc_sig     : signed (9 downto 0);
-  signal acc_p2      : signed (9 downto 0) := (others => '0');
+  signal acc_sig     : signed(9 downto 0);
+  signal acc_p2      : signed(9 downto 0) := (others => '0');
   signal count_equ_3 : std_logic;
   signal valid_p2    : std_logic := '0';
 
@@ -147,39 +147,33 @@ begin
   -- Generating the addr
 
   with count_p1 select
-    addr <= data_3_p1(0) & data_2_p1(0) & data_1_p1(0) & data_0_p1(0) when "00",
-            data_3_p1(1) & data_2_p1(1) & data_1_p1(1) & data_0_p1(1) when "01",
-            data_3_p1(2) & data_2_p1(2) & data_1_p1(2) & data_0_p1(2) when "10",
-            data_3_p1(3) & data_2_p1(3) & data_1_p1(3) & data_0_p1(3) when "11",
+    addr <= data_0_p1(0) & data_1_p1(0) & data_2_p1(0) & data_3_p1(0) when "00",
+            data_0_p1(1) & data_1_p1(1) & data_2_p1(1) & data_3_p1(1) when "01",
+            data_0_p1(2) & data_1_p1(2) & data_2_p1(2) & data_3_p1(2) when "10",
+            data_0_p1(3) & data_1_p1(3) & data_2_p1(3) & data_3_p1(3) when "11",
             (others => '0') when others;
 
-  -- process (count_p1, data_lut, acc_p2)
-  --   -- variable addr_v : unsigned(3 downto 0);
-  -- begin
-  --   case count_p1 is
-  --     when "00" => addr <= data_3_p1(0) & data_2_p1(0) & data_1_p1(0) & data_0_p1(0);
-  --     when "01" => addr <= data_3_p1(1) & data_2_p1(1) & data_1_p1(1) & data_0_p1(1);
-  --     when "10" => addr <= data_3_p1(2) & data_2_p1(2) & data_1_p1(2) & data_0_p1(2);
-  --     when "11" => addr <= data_3_p1(3) & data_2_p1(3) & data_1_p1(3) & data_0_p1(3);
-  --     when others => null; -- shouldn't happen
-  --   end case;
-  --   -- addr <= addr_v;
-  --   -- data_lut <= ROM(addr);
-  --   -- acc_p2 <= acc_p2 + data_lut;
-
-  -- end process;
+  data_lut <= ROM(addr);
 
   -- we use a dedicated signal here because the conversion from boolean to std_logic is bit complicated
-  -- in VHDL for the '*' line below ('*' line ???)
   count_equ_3 <= '1' when count_p1 = to_unsigned(3, 2) else '0';
 
-  DATA_OUT_p : process (CLK) is
+  DATA_OUT_p : process (CLK, RST) is
+    variable data_lut_shift : signed(9 downto 0);
   begin
       if rising_edge(CLK) then
         if (RST = '1') then
           valid_p2 <= '0';
+          acc_p2 <= (others => '0');
         else
           valid_p2 <= count_equ_3;
+          data_lut_shift := resize(data_lut, data_lut_shift'length);
+          data_lut_shift := shift_left(data_lut_shift, to_integer(count_p1));
+          if (count_p1 = "00") then
+            acc_p2 <= data_lut_shift;
+          else
+            acc_p2 <= acc_p2 + data_lut_shift;
+          end if;
         end if;
       end if;
   end process DATA_OUT_p;
